e224dfc5588a1fe810e5804629f360f9d9a77327
Laure Dassy <laure.dassy@hotmail.com>
2018-06-22 14:41:06 +0200

    /student2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/student2", (req, res, next) => {
+  res.send("LEWD");
 });
 
 module.exports = router;
